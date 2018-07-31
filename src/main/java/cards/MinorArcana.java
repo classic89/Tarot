05f23f6acb521175a6ad21b1d5dc43999f2bd879
classic89 <retrorock8821@gmail.com>
2018-07-30 23:44:32 -0500

    adding cards

--- src/main/java/cards/MinorArcana.java
+++ src/main/java/cards/MinorArcana.java
@@ -1,27 +1,28 @@
 package cards;
 
-public class MINORArcana{
+public class MinorArcana{
     private String name;
     private String symbol;
+    private String rank;
 
-    private MINORArcana( String nameValue, String symbolValue ){
+    MinorArcana( String nameValue, String symbolValue ){
         name = nameValue;
         symbol = symbolValue;
     }
 
-    public final static MINORArcana ACE = new MINORArcana( "Ace", "a" );
-    public final static MINORArcana TWO = new MINORArcana( "Two", "2" );
-    public final static MINORArcana THREE = new MINORArcana( "Three", "3" );
-    public final static MINORArcana FOUR = new MINORArcana( "FOUR", "4" );
-    public final static MINORArcana FIVE = new MINORArcana( "FIVE", "5" );
-    public final static MINORArcana SIX = new MINORArcana( "SIX", "6" );
-    public final static MINORArcana SEVEN = new MINORArcana( "SEVEN", "7" );
-    public final static MINORArcana EIGHT = new MINORArcana( "EIGHT", "8" );
-    public final static MINORArcana NINE = new MINORArcana( "NINE", "9" );
-    public final static MINORArcana TEN = new MINORArcana( "TEN", "t" );
-    public final static MINORArcana PAGE = new MINORArcana( "PAGE", "p" );
-    public final static MINORArcana JACK = new MINORArcana( "JACK", "j" );
-    public final static MINORArcana QUEEN = new MINORArcana( "QUEEN", "q" );
-    public final static MINORArcana KING = new MINORArcana( "KING", "k" );
+    public final static MinorArcana ACE = new MinorArcana( "Ace", "a" );
+    public final static MinorArcana TWO = new MinorArcana( "Two", "2" );
+    public final static MinorArcana THREE = new MinorArcana( "Three", "3" );
+    public final static MinorArcana FOUR = new MinorArcana( "FOUR", "4" );
+    public final static MinorArcana FIVE = new MinorArcana( "FIVE", "5" );
+    public final static MinorArcana SIX = new MinorArcana( "SIX", "6" );
+    public final static MinorArcana SEVEN = new MinorArcana( "SEVEN", "7" );
+    public final static MinorArcana EIGHT = new MinorArcana( "EIGHT", "8" );
+    public final static MinorArcana NINE = new MinorArcana( "NINE", "9" );
+    public final static MinorArcana TEN = new MinorArcana( "TEN", "t" );
+    public final static MinorArcana PAGE = new MinorArcana( "PAGE", "p" );
+    public final static MinorArcana JACK = new MinorArcana( "JACK", "j" );
+    public final static MinorArcana QUEEN = new MinorArcana( "QUEEN", "q" );
+    public final static MinorArcana KING = new MinorArcana( "KING", "k" );
 
 }
